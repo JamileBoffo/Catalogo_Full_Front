5589426fd295fac2a183e3097139187bafed18e1
Jamile Boffo <97992778+JamileBoffo@users.noreply.github.com>
2022-04-27 23:09:17 -0300

    Create movies.js

---
+++ inmovie-catalogo/src/mocks/movies.js
@@ -0,0 +1,32 @@
+export const movies = [{
+    titulo: "A Batalha Esquecida",
+    sinopse: "Durante a Segunda Guerra, na crucial Batalha do Rio Escalda, três vidas totalmente diferentes se cruzam e cada um vê o conflito sob perspectivas diferentes.",
+    img: require("../assets/img/abatalhaesquecida.jpg"),
+    ano:2020
+},{
+    titulo: "Alerta Vermelho",
+    sinopse: "Um alerta vermelho da Interpol é emitido e o agente do FBI John Hartley assume o caso. Durante sua busca, ele se vê diante de um assalto ousado e é forçado a se aliar ao maior ladrão de arte da história, Nolan Booth, para capturar a ladra de arte mais procurada do mundo atualmente, Sarah Black.",
+    img: require("../assets/img/alertavermelho.jpg"),
+    ano:2021
+},{
+    titulo: "Amor e Monstros",
+    sinopse: "Sete anos após sobreviver ao apocalipse dos monstros, o azarado Joel Dawson decide se arriscar e abandona o seu bunker para se reencontrar com a sua ex-namorada da escola, Aimee.'",
+    img: require("../assets/img/amoremonstros.jpg"),
+    ano:2020
+},{
+    titulo: "Mentiras Perigosas",
+    sinopse: "Ao se tornar a única herdeira de um paciente, a cuidadora Katie acaba envolta em uma rede de mentiras e assassinato. Para sobreviver, ela não pode confiar em ninguém – nem mesmo nas pessoas que ama.",
+    img: require("../assets/img/mentirasperigosas.jpg"),
+    ano: 2020
+},{
+    titulo: "O Menino que Descobriu o Vento",
+    sinopse: "Sempre esforçando-se para adquirir conhecimentos cada vez mais diversificados, um jovem de Malawi se cansa de assistir todos os colegas de seu vilarejo passando por dificuldades e começa a desenvolver uma inovadora turbina de vento.",
+    img: require("../assets/img/omeninodovento.jpg"),
+    ano: 2020
+}]
+
+
+
+
+
+
